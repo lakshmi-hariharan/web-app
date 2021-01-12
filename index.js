831bbf168a8a53b2928a8c4664da125e01c5bf2f
lakshmi-hariharan <48873345+lakshmi-hariharan@users.noreply.github.com>
2021-01-12 09:47:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(
 );
 
 
-const expenses = [
+/*const expenses = [
   {
     date: new Date(),
     description: "Pizza for a Coding Dojo session.",
@@ -58,15 +58,29 @@ const expenses = [
     description: "Coffee for a Coding Dojo session.",
     value: 42,
   },
-];
+];*/
 
-app.get("/", async (req, res) => {
+/*app.get("/", async (req, res) => {
   res.render("home", {
     user: req.oidc && req.oidc.user,
     total: expenses.reduce((accum, expense) => accum + expense.value, 0),
     count: expenses.length,
   });
+});*/
+
+app.get("/", async (req, res) => {
+ try {
+   const summary = await axios.get(`${API_URL}/total`);
+   res.render("home", {
+     user: req.oidc && req.oidc.user,
+     total: summary.data.total,
+     count: summary.data.count,
+   });
+ } catch (err) {
+   next(err);
+ }
 });
+
 
 // 👇 add requiresAuth middlware to these private routes  👇
 
@@ -79,12 +93,25 @@ app.get("/user", requiresAuth(), async (req, res) => {
   });
 });
 
-app.get("/expenses", requiresAuth(), async (req, res, next) => {
+/*app.get("/expenses", requiresAuth(), async (req, res, next) => {
   res.render("expenses", {
     user: req.oidc && req.oidc.user,
     expenses,
   });
-});
+});*/
+
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
+   try {
+     const expenses = await axios.get(`${API_URL}/reports`);
+     res.render("expenses", {
+       user: req.oidc && req.oidc.user,
+       expenses: expenses.data,
+     });
+   } catch (err) {
+     next(err);
+   }
+ });
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
