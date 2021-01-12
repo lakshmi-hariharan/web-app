b53d2991209e584ce84799f2d01d1e4e110f845c
lakshmi-hariharan <48873345+lakshmi-hariharan@users.noreply.github.com>
2021-01-12 10:10:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ app.use(
    authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
+   // 👇 add this 👇
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
+   // 👆 add this 👆
+
  })
 );
 
@@ -104,6 +111,16 @@ app.get("/user", requiresAuth(), async (req, res) => {
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
    try {
+     // 👇 get the token from the request 👇
+   const { token_type, access_token } = req.oidc.accessToken;
+   // 👇 then send it as an authorization header 👇
+   const expenses = await axios.get(`${API_URL}/reports`, {
+     headers: {
+       Authorization: `${token_type} ${access_token}`,
+     },
+   });
+   // 👆 end of changes 👆
+
      const expenses = await axios.get(`${API_URL}/reports`);
      res.render("expenses", {
        user: req.oidc && req.oidc.user,
